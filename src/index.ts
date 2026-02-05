ba5f5144bf374fefbb05cf4367512704865c0681
Дастан <dastan@example.com>
2026-02-05 15:20:16 +0600

    fix express listen (render port binding)

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ const app = buildServer();
 const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`server started on port ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
